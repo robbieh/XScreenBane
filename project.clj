fb1c2868c8d88122ea1e62e9f555ce6d37fdf544
Robbie <robbie.huffman@nundrum.net>
2024-11-05 16:28:01 -0500

    bumped Clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url ""
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [net.java.dev.jna/jna "5.9.0"]
                  [net.java.dev.jna/jna-platform "5.9.0"]
                  [clojure2d "1.4.5"]
