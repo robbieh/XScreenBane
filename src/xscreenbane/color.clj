9f5459efcd0352436728ffcf530a2bd3c7072a63
Robbie <robbie.huffman@nundrum.net>
2024-11-05 16:29:00 -0500

    first pass at handling palette config

--- src/xscreenbane/color.clj
+++ src/xscreenbane/color.clj
@@ -1,26 +1,44 @@
 (ns xscreenbane.color
+  (:use com.rpl.specter)
   (:require [clojure.set]
-            [xscreenbane.config :refer [config]]))
+            [clojure.java.io :as io]
+            [clojure.edn :as edn]
+            [clojure2d.color :as c2color]
+            [xscreenbane.config :refer [config]])
+  (:import [java.io PushbackReader]))
 
 
-(def gradients "map containing gradients in form ..." ;TODO
-               {
-                :grayscale [[0 0 0] [255 255 255]]
-                :greens    [[127 255 0] [0 255 127]]
-                :greenpunk []
+;(edn/read-string {:readers {'xsc/color c2color/color }} 
+;"{:line #xsc/color \"#00f000\"}")
 
-                })
+;(edn/read-string {:readers {'color identity}} "#color 'test'")
+;(Color/decode "000000")
 
 
-(def palettes  "map containing palettes in form ..." ;TODO
-               {
-                :greens    []
-                :greenpunk []
+(def palettes 
+  (let [config-files [
+                     (io/file (System/getenv "HOME") ".xscreenbane-palettes.edn")
+                     (io/file (System/getenv "HOME") ".config" "xscreenbane" "palettes.edn")
+                     (io/file (System/getenv "XDG_CONFIG_HOME") "xscreenbane" "palettes.edn")
+                     (io/file (io/resource "palettes.edn"))
+                   ]]
+  (->> config-files 
+     (filter #(.exists %))
+     (mapv io/reader)
+     (mapv #(PushbackReader. %))
+     (mapv edn/read)
+     ;add a spec step here, once the spec is known
+     (apply merge)
+     ;(select  [(recursive-path [] p (if-path map? (continue-then-stay MAP-VALS p))) MAP-VAL])
+     ;(select  [(recursive-path [] p (if-path map? [p MAP-VALS])) ])
+     ;(select  [(recursive-path [] p (if-path map? [MAP-VALS p] [STAY]))] )
+     (transform [(recursive-path [] nxt (if-path map? [MAP-VALS nxt] [STAY])) (if-path vector? [ALL] [STAY])] c2color/color)
+     ;(transform [(recursive-path [] p (if-path map? (continue-then-stay MAP-VALS p))) k c2color/color )
+     
+     ))
+  )
 
-               })
-(def color-sets "List of keywords representing entires in both `gradients` and `palettes`"
-  (let [gs (keys gradients)
-        ps (keys palettes)]
-    (clojure.set/intersection (set gs) (set ps))))
+
+ 
 
 
